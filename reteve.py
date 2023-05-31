1bb83a0ae3b98e1f9f21530c28e1dea0cbf48d68
Guillen-30 <134899549+Guillen-30@users.noreply.github.com>
2023-05-30 23:43:13 -0600

    Prueba

--- reteve.py
+++ reteve.py
@@ -20,6 +20,8 @@ contador_citas=1
 
 #Lista de vehiculos disponibles para seleccionar
 
+#Holajajaj
+
 lista_vehiculos = [
     "Automóvil Partícular y Vehículo de Carga Liviana (Menor o Igual a 3500kg)",
     "Automóvil Partícular y Vehículo de Carga Liviana (Mayor a 3500kg pero menor a 8000kg)",
